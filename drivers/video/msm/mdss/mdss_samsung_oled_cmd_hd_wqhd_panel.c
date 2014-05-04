b2176a024cb3f3a311dfadc59d755e4666aaad83
Paul Reioux <reioux@gmail.com>
2014-05-04 15:27:40 -0700

    mdss_samsung_oled_cmd_hd_wqhd_panel.c: add powersuspend hooks
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- drivers/video/msm/mdss/mdss_samsung_oled_cmd_hd_wqhd_panel.c
+++ drivers/video/msm/mdss/mdss_samsung_oled_cmd_hd_wqhd_panel.c
@@ -42,6 +42,10 @@
 
 #include <asm/system_info.h>
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #define SMART_ACL
 #define HBM_RE
 #define TEMPERATURE_ELVSS
@@ -2257,6 +2261,10 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_pannel_hook(POWER_SUSPEND_INACTIVE);
+#endif
+
 	pr_info("%s : ++\n", __func__);
 
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
@@ -2415,6 +2423,10 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 
 end:
 	pr_info("%s : --\n",__func__);
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_pannel_hook(POWER_SUSPEND_ACTIVE);
+#endif
+
 	return 0;
 }
 
